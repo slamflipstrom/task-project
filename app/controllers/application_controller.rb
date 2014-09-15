6b7c0f1e35cffbc4b7d77ac138148d023ece4df0
slamflipstrom <lindstrom.s@gmail.com>
2014-09-15 14:58:04 -0500

    Added methods to Logins & App Controllers
    App Controller - Added Authorize method
    Logins Controller - Create, Current User, and Destroy methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
+  
+  def authorize
+    if current_user.nil?
+      redirect_to login_path
+    end
+  end
+  
   protect_from_forgery
 end
--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,2 +1,25 @@
 class LoginsController < ApplicationController
+  
+  def create
+    user = User.find_by_email(params[:email])
+    if user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to articles_path
+    else
+      raise "Invalid login."
+    end
+  end
+  
+  def current_user
+    if session[:user_id]
+      User.find(session[:user_id])
+    end
+  end
+  helper_method :current_user
+  
+  def destroy
+    session[:user_id] = nil # Could also call `reset_session` to clear the entire session.
+    redirect_to articles_path
+  end
+
 end
