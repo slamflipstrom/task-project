4c899f298588e7937ff4f293ab8bbf69d8818f93
Heather Praiswater <heathered@gmail.com>
2014-09-15 15:10:32 -0500

    Add current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,18 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery
+  helper_method :current_user
+  before_filter :authorize
   
-  def authorize
-    if current_user.nil?
-      redirect_to login_path
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
     end
   end
   
-  protect_from_forgery
+  def authorize
+    if current_user.nil?
+      redirect_to login_path, :alert => "You need to log in to do that."
+    end
+  end
+
 end
