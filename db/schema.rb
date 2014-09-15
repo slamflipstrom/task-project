2f210bb9896e97c64dba4ee41c51d169197114cd
“Jason <j.lukasson@gmail.com>
2014-09-15 14:40:09 -0500

    database created

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140915192336) do
+ActiveRecord::Schema.define(:version => 20140915192723) do
+
+  create_table "tasks", :force => true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.boolean  "done"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
