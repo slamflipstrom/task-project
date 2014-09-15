906fac94cf61e4554daff6f5194bb2785b540f95
“Jason <j.lukasson@gmail.com>
2014-09-15 14:51:56 -0500

    creating contoller actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,18 @@
 class TasksController < ApplicationController
+  
+  def index
+    @tasks=Task.all
+  end
+  
+  def show
+    @task=Task.find(params[:id])
+  end
+  
+  def new
+    @tasks=Task.new
+  end
+  
+  def create
+  end
+  
 end
