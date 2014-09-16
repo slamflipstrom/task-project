1b8718e2532067f48e0e1e050d7d62f60bf28477
“Jason <j.lukasson@gmail.com>
2014-09-15 20:47:18 -0500

    Add category_id to tasks. Create category index, show and new views.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140916000456) do
+ActiveRecord::Schema.define(:version => 20140916014249) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(:version => 20140916000456) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.integer  "user_id"
+    t.integer  "category_id"
   end
 
   create_table "users", :force => true do |t|
