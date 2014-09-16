6076f5aebfc1b2225c4980647b27a4c626a5dcf7
Heather Praiswater <heathered@gmail.com>
2014-09-16 16:46:12 -0500

    Bug fixing.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_and_belongs_to_many :projects
-  has_many :tasks, :categories
+  has_many :tasks
+  has_many :categories
   
   has_secure_password 
 end
