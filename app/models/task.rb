fb298b0ba53c26408617d984a0798d0525069636
“Jason <j.lukasson@gmail.com>
2014-09-16 16:50:33 -0500

    fix bugs and other small

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ActiveRecord::Base
   attr_accessible :description, :done, :title, :user_id, :category_id
-  belongs_to :category :project
+  belongs_to :category
+  belongs_to :project
   
   validates :title, :presence => true
 end
