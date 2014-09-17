d1206356e6a65d168dcdaff9fa18c2d7efb57316
slamflipstrom <lindstrom.s@gmail.com>
2014-09-17 16:53:18 -0500

    Debugged task_path errors in comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -20,7 +20,7 @@ class CommentsController < ApplicationController
       if @comment.save
         u=User.find(session[:user_id])
         u.comments << @comment
-        redirect_to task_path(@comment.task_id)
+        redirect_to task_path(@comment.task.url)
       else
         render 'tasks#new'
       end
@@ -39,7 +39,7 @@ class CommentsController < ApplicationController
     @comment = Comment.find(params[:id])
     @comment.destroy
     
-    redirect_to task_path(@comment.task_id)
+    redirect_to task_path(@comment.task.url)
   end
   
 end
