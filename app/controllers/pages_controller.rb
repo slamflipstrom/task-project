b88ce6170db3cee11a8f9f9054f4af0a770cba23
Heather Praiswater <heathered@gmail.com>
2014-09-17 16:58:48 -0500

    Session!

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,6 @@ class PagesController < ApplicationController
   skip_before_filter :authorize, :only => [:index]
   
   def index
-    session[:user_id] = nil
     
     if current_user
       @tasks = Task.where({:user_id => session[:user_id]})
