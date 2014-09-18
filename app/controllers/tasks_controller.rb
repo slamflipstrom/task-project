97c728ce2cfb18be73361d9bf19e0cf70c112149
“Jason <j.lukasson@gmail.com>
2014-09-18 12:26:34 -0500

    modified comments, projects and tasks controller to move destroy method call on instance

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -83,8 +83,8 @@ class TasksController < ApplicationController
   
   def destroy
     @task = Task.find(params[:id])
-    @task.destroy
     @task.create_activity :destroy, owner: current_user
+    @task.destroy
     
     redirect_to tasks_path
   end
