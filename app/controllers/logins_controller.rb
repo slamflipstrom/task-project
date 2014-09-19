c5892f6793b9187c091982ff891674bccc1c15e3
“Jason <j.lukasson@gmail.com>
2014-09-19 13:37:20 -0500

    modified logins controller to handle no email provided

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -3,11 +3,11 @@ class LoginsController < ApplicationController
   
   def create
     user = User.find_by_email(params[:email])
-    if user.authenticate(params[:password])
+    if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root_path
     else
-      raise "Invalid login."
+      redirect_to login_path, :alert => "Login Invalid"
     end
   end
   
